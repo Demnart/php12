eca8489d9f2eb0a02a7ed1ddfe2087359567df72
genjo <insidia93@gmail.com>
2017-06-02 11:41:23 +0300

    начальное обучение ооп

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: genjo
- * Date: 31.05.17
- * Time: 11:38
- */
+
+//class Article{
+//
+//  public $title;
+//  public $text;
+//
+//  public function __construct($title,$text)
+//  {
+//      $this->title = $title;
+//      $this->text = $text;
+//  }
+//
+//    public function view()
+//  {
+//      echo $this->title . '<br>'. $this->text;
+//  }
+//
+//}
+//
+//$res = new Article('Любовь','Не');
+//$res->view();
+//echo '<br>';
+//$news = new Article('Сон','Работа');
+//$news->view();
+//
