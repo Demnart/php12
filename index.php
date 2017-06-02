746bf7f040a25181402a10cdb7a13910e61c5120
genjo <insidia93@gmail.com>
2017-06-02 11:43:39 +0300

    начальное обучение ооп

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
 
-//class Article{
-//
-//  public $title;
-//  public $text;
-//
-//  public function __construct($title,$text)
-//  {
-//      $this->title = $title;
-//      $this->text = $text;
-//  }
-//
-//    public function view()
-//  {
-//      echo $this->title . '<br>'. $this->text;
-//  }
-//
-//}
-//
-//$res = new Article('Любовь','Не');
-//$res->view();
-//echo '<br>';
-//$news = new Article('Сон','Работа');
-//$news->view();
-//
+class Article{
+
+  public $title;
+  public $text;
+
+  public function __construct($title,$text)
+  {
+      $this->title = $title;
+      $this->text = $text;
+  }
+
+    public function view()
+  {
+      echo $this->title . '<br>'. $this->text;
+  }
+
+}
+
+$res = new Article('Любовь','Не');
+$res->view();
+echo '<br>';
+$news = new Article('Сон','Работа');
+$news->view();
+
+
