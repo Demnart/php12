139e79d47c3891d4378667a59eecad987960ea62
genjo <insidia93@gmail.com>
2017-06-02 15:47:18 +0300

    Наследование и полиморфизм

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php
 
-class Article{
-
-  public $title;
-  public $text;
-
-  public function __construct($title,$text)
-  {
-      $this->title = $title;
-      $this->text = $text;
-  }
-
-    public function view()
-  {
-      echo $this->title . '<br>'. $this->text;
-  }
-
-}
-
-$res;
+//class Article{
+//
+//  public $title;
+//  public $text;
+//
+//  public function __construct($title,$text)
+//  {
+//      $this->title = $title;
+//      $this->text = $text;
+//  }
+//
+//    public function view()
+//  {
+//      echo $this->title . '<br>'. $this->text;
+//  }
+//
+//}
+//
+//class NewsArticle extends  Article
+//{
+//
+//}
+//
+//$test = new NewsArticle('title','text');
+//$test ->view();
