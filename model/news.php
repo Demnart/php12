dcbbe0e3e07b44aeae33a58f321ad63f6faf048f
genjo <insidia93@gmail.com>
2017-06-05 13:45:52 +0300

    Создали класс новостей, который наследуется от класса статья и реализует абстрактные методы

--- model/news.php
+++ model/news.php
@@ -1,28 +1,28 @@
 <?php
 require __DIR__ . '/../functions/mysql.php';
+require __DIR__ . '/abstrnews.php';
 
-class News extends AbstractNews
+class News extends Article
 {
-    function getAllNews()
-    {
+     function getAllNews(){
         $resource = new Mysql();
         $sql = "SELECT * FROM news";
         return $resource->getDataFromDB($sql);
     }
-
-    function addNewsToDB()
-    {
-        // TODO: Implement addNewsToDB() method.
-    }
-
-    function getOneNews()
-    {
-        // TODO: Implement getOneNews() method.
-    }
-
-    function updateNews()
-    {
-        // TODO: Implement updateNews() method.
-    }
+//
+//    public  function addNewsToDB()
+//    {
+//
+//    }
+//
+//    public function getOneNews()
+//    {
+//
+//    }
+//
+//    public function updateNews()
+//    {
+//
+//    }
 
 }
