70491fe2c3bb484c77ebc746022d29546997469b
genjo <insidia93@gmail.com>
2017-06-05 10:24:07 +0300

    создали абстрактный класс новостей

---
+++ model/abstrnews.php
@@ -0,0 +1,11 @@
+<?php
+
+class AbstractNews
+{
+
+    abstract function getAllNews();
+    abstract function addNewsToDB();
+    abstract function getOneNews();
+    abstract function updateNews();
+
+}
