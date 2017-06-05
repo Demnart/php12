88a596103d37b15df3dc06812d192392a8614cb3
genjo <insidia93@gmail.com>
2017-06-05 17:18:57 +0300

    Добавили ссылку на нашу будущую форму

--- view/index.php
+++ view/index.php
@@ -3,3 +3,12 @@
 foreach ($result as $value){
     echo $value['title'] . '<br>' . $value['text'] . '<br>' . $value['author'];
 }
+?>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<a href="/view/add.php">Добавить новость</a>
+</body>
+</html>
