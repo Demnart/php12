50d35921fc37f92b8f56d81cbe9f032722635bcb
genjo <insidia93@gmail.com>
2017-06-05 17:20:18 +0300

    Добавили файл нашей формы и ссылку

---
+++ view/add.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<a href="/index.php">На главную</a>
+</body>
+</html>
