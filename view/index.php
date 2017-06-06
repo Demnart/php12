de9f68a331adcc88a4aeb9c758d91e30c5d0e9b3
genjo <insidia93@gmail.com>
2017-06-06 12:04:39 +0300

    Наша вьюшка приобретает нормальный вид

--- view/index.php
+++ view/index.php
@@ -1,14 +1,21 @@
-<?php
-
-foreach ($result as $value){
-    echo $value['title'] . '<br>' . $value['text'] . '<br>' . $value['author'];
-}
-?>
 <html>
 <head>
     <title></title>
 </head>
 <body>
+<?php foreach ($result as $value): ?>
+<table style="border: 1px solid black; border-collapse: collapse; margin: 0 auto;">
+    <tr>
+        <th style="border: 1px solid black;"><?php echo $value['title']?></th>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black"><?php echo $value['text']?></td>
+    </tr>
+     <tr>
+         <td style="border: 1px solid black"><?php echo $value['author']?></td>
+     </tr>
+</table>
+<?php endforeach;?>
 <a href="/view/add.php">Добавить новость</a>
 </body>
 </html>
