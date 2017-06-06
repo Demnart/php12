0de432e4a0038a202b2c3d2cff79bd4e5a08d72b
genjo <insidia93@gmail.com>
2017-06-06 09:25:42 +0300

    Мы сделали наш мини контроллер для обработки данных поступающих с формы

--- controllers/add.php
+++ controllers/add.php
@@ -1,1 +1,34 @@
 <?php
+session_start();
+require __DIR__ . '/../model/news.php';
+
+if (!empty($_POST))
+{
+    $data=[];
+
+    if (empty($_POST['title']) || empty($_POST['text']) || empty($_POST['author']))
+    {
+        $_SESSION['error'] = 'Одно из полей не заполенено';
+        header('Location: /view/add.php');
+    }
+
+    if (!empty($_POST['title']))
+    {
+        $data['title'] = $_POST['title'] ;
+    }
+    if (!empty($_POST['text']))
+    {
+        $data['text'] = $_POST['text'];
+    }
+    if (!empty($_POST['author']))
+    {
+        $data['author']  = $_POST['author'];
+    }
+
+    if (isset($data['title']) && isset($data['text']) && isset($data['author']))
+    {
+        $resource = new News();
+        $resource->addNewsToDB($data);
+        header('Location: /');
+    }
+}
