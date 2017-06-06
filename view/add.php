5a0bdad80647e776c0acd65a2db29ffcd0afafa2
genjo <insidia93@gmail.com>
2017-06-06 09:26:08 +0300

    Создали форму для добавления новостей

--- view/add.php
+++ view/add.php
@@ -8,7 +8,28 @@
     <title>Title</title>
 </head>
 <body>
-
-<a href="/index.php">На главную</a>
+<form action="/controllers/add.php" method="post">
+<table style="margin: 0 auto; border: 1px solid black;border-collapse: collapse;text-align: center">
+    <tr>
+        <th style="border: 1px solid black" colspan="3">Добавьте новость</th>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black"><label for="title">Название новости :</label></td>
+        <td style="border: 1px solid black" colspan="2"><input type="text" placeholder="Название новости" id="title" name="title"></td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black"><label for="text">Текст новости</label></td>
+        <td style="border: 1px solid black" colspan="2"><textarea name="text" id="text" cols="30" rows="10"></textarea></td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black"><label for="autor">Автор</label></td>
+        <td style="border: 1px solid black" colspan="2"><input type="text" id="autor" name="author" placeholder="Автор"></td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid black" colspan="3"><input type="submit" value="Отправить"></td>
+    </tr>
+</table>
+</form>
+<p style="text-align: center"><a href="/index.php">На главную</a></p>
 </body>
 </html>
